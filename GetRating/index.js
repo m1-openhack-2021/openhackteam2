c66698a1c1c2885457fd50c35dc9eb2ee2186588
Edwin Ng Shyang Wei <edwin_ng_1984@hotmail.com>
2021-06-03 14:31:18 +0800

    update functions js

--- GetRating/index.js
+++ GetRating/index.js
@@ -1,13 +1,50 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+// Azure Function: Node.js code to read PostgreSQL data and return results as JSON
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+// Import the pg (node-postgres) library
+const pg = require('pg');
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+// Entry point of the function
+module.exports = async function(context, req) {
+
+    // Define variables to store connection details and credentials
+    const config = {
+        host: process.env["POSTGRES_SERVER_NAME"],
+        user: process.env["POSTGRES_USER_NAME"],
+        password: process.env["POSTGRES_USER_PASS"],
+        database: 'icecream',
+        port: 5432,
+        ssl: true
     };
+
+    // Create query to execute against the database
+    const querySpec = {
+        //text: 'SELECT * FROM Rating'
+       text: "SELECT * FROM Rating WHERE Id='" + req.query.ratingId + "'"
+    }
+
+    try {
+        // Create a pool of connections
+        const pool = new pg.Pool(config);
+
+        // Get a new client connection from the pool
+        const client = await pool.connect();
+
+        // Execute the query against the client
+        const result = await client.query(querySpec);
+
+        // Release the connection
+        client.release();
+
+        // Return the query resuls back to the caller as JSON
+        context.res = {
+            status: 200,
+            isRaw: true,
+            body: result.rows[0],
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+    } catch (err) {
+        context.log(err.message);
+    }
 }
